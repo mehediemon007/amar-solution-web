ec4ddc076a9ce56789cc5265e7d481cfd2f87d46
mehediemon007 <mehediemon7@gmail.com>
2021-11-18 19:10:04 +0600

    fixing

--- js/main.js
+++ js/main.js
@@ -18,16 +18,51 @@
        sidebar.classList.remove("active")
    })
 
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    if(page=='index.html'){
+
+        var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+
+        window.onscroll = () =>{
+
+            this.scrollY > 100 ? headerContainer.classList.add("sticky") : headerContainer.classList.remove("sticky"); 
+
+            var percent = Math.round((h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 10);
+            if(percent <=3 ){
+                document.querySelector("body").style.backgroundColor = 'rgba(' + 10 + ', ' + 149 + ', ' + 249 + ', ' + '.'+ ( percent + 1 ) + ')';
+            }
+            else if(percent >=4 && percent <= 6){
+                document.querySelector("body").style.backgroundColor = 'rgba(' + 10 + ', ' + 149 + ', ' + 249 + ', ' + '.9' + ')';
+            }
+            else if( percent ==8){
+                document.querySelector("body").style.backgroundColor = 'rgba(' + 10 + ', ' + 149 + ', ' + 249 + ', ' + '.4'+ ')';
+            }
+            else if( percent ==9){
+                document.querySelector("body").style.backgroundColor = 'rgba(' + 10 + ', ' + 149 + ', ' + 249 + ', ' + '.3'+ ')';
+            }
+            else if( percent ==10){
+                document.querySelector("body").style.backgroundColor = 'rgba(' + 10 + ', ' + 149 + ', ' + 249 + ', ' + '.2'+ ')';
+            }
+        }    
+    }
+
 })()
 
+;(function($){
 
+    $(".toggle-2").click(function(){
+        $(".ams-main-nav").slideToggle("100");
+    })
+    
+    $("li.has-children > a").click(function(e){
+        e.preventDefault();
+        $(this).next(".sub-menu").slideToggle("100")
+    
+    })
 
-$(".toggle-2").click(function(){
-    $(".ams-main-nav").slideToggle("100");
-})
-
-$("li.has-children > a").click(function(e){
-    e.preventDefault();
-    $(this).next(".sub-menu").slideToggle("100")
-
-})
+})(jQuery);
