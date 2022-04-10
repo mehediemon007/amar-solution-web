17a384fbed18f47c92a5890937b90b91af48d525
mehediemon007 <mehediemon7@gmail.com>
2022-04-10 16:53:32 +0600

    fix

--- js/main.js
+++ js/main.js
@@ -154,7 +154,8 @@
        nav:true,
        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
        autoplay:true,
-       autoplayTimeout:3000
+       autoplayTimeout:2000,
+       smartSpeed:3000
      })
    }
 
